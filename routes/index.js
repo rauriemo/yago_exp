5e518a7fc150dee9d37ed98824fe417eb8b19b0b
rauriemo <rafael.auriemo@gmail.com>
2015-05-10 12:39:44 -0700

    first app.js structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-module.exports = router;
+module.exports = app;
