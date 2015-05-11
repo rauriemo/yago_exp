ef9410d6a0fe331e71da6a215b543dbd6b159730
Dillon Downey <downeydillon@gmail.com>
2015-05-10 23:42:15 -0700

    Added rough draft of keyMap for Nav

--- plugins/nav/public/js/nav.js
+++ plugins/nav/public/js/nav.js
@@ -3,10 +3,85 @@
 
   'use strict';
   var keyMap = {
+    // " " <-- empty string
+    32 : {
+            ev: 'drone'
+            action: 'stop'
+    },
+    // T "takeoff"
+    84 : {
+            ev: 'drone'
+            action: 'takeoff'
+    },
+    // L "land"
     76 : {
-            ev : 'drone',
-            action : 'takeoff'
-          },
+            ev: 'drone',
+            action: 'land'
+    },
+    // Left arrow "turn counterclockwise"
+    37 : {
+            ev: 'move'
+            action: 'counterClockwise'
+    },
+    // Right arrow "turn clockwise"
+    39 : {
+            ev: 'move'
+            action: 'clockwise'
+    },
+    // Up arrow "move vertically up"
+    38 : {
+            ev: 'move'
+            action: 'up'
+    },
+    // Down arrow "move vertically down"
+    40 : {
+            ev: 'move'
+            action: 'down'
+    },
+    // W "move forward"
+    87 : {
+            ev: 'move'
+            action: 'front'
+    },
+    // S "move backward"
+    83 : {
+            ev: 'move'
+            action: 'back'
+    },
+    // A "move left"
+    65: {
+            ev: 'mpve'
+            action: 'left'
+    },
+    // D "move right"
+    68: {
+            ev: 'move'
+            action: 'right'
+    },
+    // T "flip forward"
+    84 : {
+            ev: 'animate'
+            action:
+    },
+    // G "flip backward"
+    71 : {
+            ev: 'animate'
+            action:
+    },
+    // H "flip right"
+    72 : {
+            ev: 'animate'
+            action:
+    },
+    // F  "flip left"
+    70 : {
+            ev: 'animate'
+            action:
+    },
+    // C "Change camera channel"
+    67 : {
+            ev: 'channel'
+    }
   }
 
   var Nav = function() {
