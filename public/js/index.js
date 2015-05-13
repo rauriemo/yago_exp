5cb542f77b42194138c25377781f415778e3f081
Grace Yasukawa <grace.yasukawa@gmail.com>
2015-05-13 11:57:42 -0700

    added non-working code to blink the rec button and made some styling changes to the command-placeholder

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,58 @@
 $(document).ready(function(){
-  $('div.button').toggleClass('opacity-switch');
-  $('#command-placeholder').toggleClass('opacity-switch')
+  $('div.button').toggleClass('opacity-off');
+  $('#command-placeholder').toggleClass('opacity-off')
   console.log("in the index.js file")
   $('body').keydown(function(event){
     console.log("in the keydown")
     if (event.which == 84){
       console.log("pressed t")
       $('#yago-drop-down').slideUp("slow");
-      $('#commands').slideUp();
-      $('#command-placeholder').removeClass('opacity-switch');
+      $('#commands').slideUp("slow");
+      $('#command-placeholder').removeClass('opacity-off');
     }
     else if (event.which == 76) {
       console.log("pressed l")
       $('#yago-drop-down').slideDown("slow");
-      $('#commands').slideUp();
-      $('#command-placeholder').removeClass('opacity-switch');
+      $('#commands').slideUp("slow");
+      $('#command-placeholder').removeClass('opacity-off');
     }
     else if (event.which == 67) {
       console.log("pressed c")
-      $('#commands').slideToggle()
+      $('#commands').slideToggle("slow")
 
-      $('#command-placeholder').toggleClass('opacity-switch')
+      $('#command-placeholder').toggleClass('opacity-off')
     }
     else if (event.which == 82) {
       console.log("pressed r")
       // $('.button').attr('visibility', 'visible');
-      $('div.button').toggleClass('opacity-switch');
+      $('div.button').toggleClass('opacity-off');
+      var show;
+      if ($('div.button').attr('class') === 'button') {
+        function showing() {
+          show = setTimeOut(fadeLoop())
+        }
+      }
+      else {
+        function clearing() {
+          clearTimeOut(show)
+        }
+      }
+      // until (event.which == 82) {
+      //   fadeLoop();
+      // }
+
       // toggleAttr('.button', 'opacity', [0,1]);
     }
 
   }); //84 is T, 76 is L
+
+
+
+  var fadeLoop = function() {
+    $('div.button').fadeIn(1500, function() {
+      $('div.button').fadeOut(1500, fadeLoop)
+    })
+  }
 
 // function toggleAttr(el, attribute, vals) {
 //   if ($(el).attr(attribute) == vals[0]) {
